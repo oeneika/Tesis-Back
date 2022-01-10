c2c6ec84560db227a161d0f4651a9d5fd219855a
Alexander De Azevedo <alexander_deazevedo@hotmail.com>
2022-01-10 12:06:12 -0400

    Subiendo setAuthentication

--- models/user.js
+++ models/user.js
@@ -4,37 +4,47 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var UserSchema = Schema({
-	name: {
-		type: String,
-		trim: true,
-	},
-	surname: {
-		type: String,
-		trim: true,
-	},
-	email: {
-		type: String,
-		required: true,
-		trim: true,
-	},
-	password: {
-		type: String,
-		required: true,
-		trim: true,
-	},
-	temp_secreto: Object,
-	birthday: String,
-	country: String,
-	image: String,
-	roles: [{type: Schema.ObjectId, ref: "Role", required: true}],
-	user_camera: {
-		type: Schema.ObjectId,
-		ref: "UserCamera",
-	},
-	user_room: [{
-		type: String,
-		required: false,
-	}]
+    name: {
+        type: String,
+        trim: true,
+    },
+    surname: {
+        type: String,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    temp_secreto: Object,
+    birthday: String,
+    country: String,
+    image: String,
+    hasTwoStepsAuth: {
+        type: Boolean,
+        trim: false,
+        required: false,
+    },
+    hasSetTwoSteps: {
+        type: Boolean,
+        trim: false,
+        required: false,
+    },
+    roles: [{ type: Schema.ObjectId, ref: "Role", required: true }],
+    user_camera: {
+        type: Schema.ObjectId,
+        ref: "UserCamera",
+    },
+    user_room: [{
+        type: String,
+        required: false,
+    }, ],
 });
 
 module.exports = mongoose.model("User", UserSchema);
--- routes/user.js
+++ routes/user.js
@@ -11,32 +11,33 @@ const md_auth_admin = require("../middleware/is_admin");
 const md_auth_collaborator = require("../middleware/is_collaborator");
 
 var multipart = require("connect-multiparty");
-var md_upload = multipart({uploadDir: "./uploads/users"});
+var md_upload = multipart({ uploadDir: "./uploads/users" });
 
-var {check} = require("express-validator");
+var { check } = require("express-validator");
 
 // Protegemos la ruta con autenticación para ver si el usuario está logueado (md_auth.ensureAuth)
 api.post("/register", UserController.saveUser);
 api.post("/login", UserController.login);
 api.post("/verify", UserController.verificationCode);
 api.put(
-	"/update-user/:id",
-	[md_auth.ensureAuth, md_auth_admin.isAdmin],
-	UserController.updateUser
+    "/update-user/:id", [md_auth.ensureAuth, md_auth_admin.isAdmin],
+    UserController.updateUser
 );
 api.get("/get-user/:id", [md_auth.ensureAuth], UserController.getUser);
 api.post(
-	"/upload-image-user/:id",
-	[md_auth.ensureAuth, md_upload],
-	UserController.uploadImage
+    "/upload-image-user/:id", [md_auth.ensureAuth, md_upload],
+    UserController.uploadImage
 );
 api.get("/get-image-file/:imageFile", UserController.getImageFile);
 api.get("/auth-user", md_auth.ensureAuth, UserController.getUserToken);
 api.put("/forgot-password", UserController.forgotPassword);
+api.put(
+    "/set-authentication/:id", [md_auth.ensureAuth, md_auth_admin.isAdmin],
+    UserController.setAuthentication
+);
 api.post(
-	"/save-collaborator",
-	[md_auth.ensureAuth, md_auth_admin.isAdmin],
-	CollaboratorController.createCollaborator
+    "/save-collaborator", [md_auth.ensureAuth, md_auth_admin.isAdmin],
+    CollaboratorController.createCollaborator
 );
 
 module.exports = api;
