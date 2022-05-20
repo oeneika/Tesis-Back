d7cdd69f598e9bfa8c0b6f6f94a28a7b0f794614
Maurizio Volpe <68517589+maurovolpe18@users.noreply.github.com>
2022-05-19 21:39:04 -0300

    hour

--- models/notification.js
+++ models/notification.js
@@ -4,6 +4,11 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var NotificationSchema = Schema({
+  hour: {
+    type: Date,
+    required: true,
+    trim: true,
+  },
   facialExpression: {
     type: String,
     required: true,
@@ -14,7 +19,7 @@ var NotificationSchema = Schema({
     required: true,
     trim: true,
   },
-  face: { type: Schema.ObjectId, ref: "Face", required: true },
+  face: { type: Schema.ObjectId, ref: "Face", required: false },
   image: { type: Schema.ObjectId, ref: "Image", required: true },
   camera: { type: Schema.ObjectId, ref: "Camera", required: true },
   user: { type: Schema.ObjectId, ref: "User", required: true },
