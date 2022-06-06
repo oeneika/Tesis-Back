0c31a83834a146e44251543ef86a0ae2d041709d
Alexander De Azevedo <alexander_deazevedo@hotmail.com>
2022-06-06 18:38:23 -0400

    Borrando elementos innecesarios

--- index.js
+++ index.js
@@ -2,15 +2,9 @@
 require('dotenv').config();
 var mongoose = require("mongoose");
 var app = require("./app");
-var port = process.env.PORT || 8000;
-
-// Url de la base de datos a https://mlab.com/databases/zoo
-//var url = "mongodb://localhost:27017/camaraDB";
+const port = process.env.PORT || 8000;
 
 var url = process.env.MONGO_CONNECTION
-
-//var url =
-//("mongodb+srv://oeneika:febrero1996@ucabcluster.lfasz.mongodb.net/tesis_backend");
 const connectionParams={
     useNewUrlParser: true,
 	useCreateIndex: true,
