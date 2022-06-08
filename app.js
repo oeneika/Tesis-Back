14e5b352e453269130051cc179d8dae3810fab69
Alexander De Azevedo <alexander_deazevedo@hotmail.com>
2022-06-08 18:12:02 -0400

    Actualizando cors

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use(cors());
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  //res.header("Access-Control-Allow-Credentials", false);
+  res.header("Access-Control-Allow-Credentials", false);
   res.header(
     "Access-Control-Allow-Headers",
     "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
