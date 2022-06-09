97a495331e1de2685a1d2c4342ebb586d47ea12b
Alexander De Azevedo <alexander_deazevedo@hotmail.com>
2022-06-08 21:39:42 -0400

    subiendo back sin images

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use((req, res, next) => {
 
 const io = require("socket.io")(http, {
   cors: {
-    origin: "*",
+    origin: process.env.FRONT_END_ORIGIN || "http://localhost:4200",
     methods: ["GET", "POST"],
     transports: ["websocket", "polling"],
     credentials: true,
